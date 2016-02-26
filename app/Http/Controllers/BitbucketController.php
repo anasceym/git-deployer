36cf2b247688c0d9f9a30593a7c43d3ae07bd924
anasceym <anasfirdauss@gmail.com>
2016-02-26 12:06:58 +0800

    WIP Bitbucket hook

--- app/Http/Controllers/BitbucketController.php
+++ app/Http/Controllers/BitbucketController.php
@@ -10,7 +10,12 @@ class BitbucketController extends Controller
 {
     private $provider = null;
 
+    /**
+     * Constructor
+     *
+     */
     public function __construct() {
+
         $this->provider = Provider::bitbucket();
 
         if(!$this->provider) {
@@ -23,10 +28,29 @@ class BitbucketController extends Controller
      * @return void
      */
     public function hookHandler(Request $request) {
+
+        // We only interested in push events
+        if($request->headers->get('X-Event-Key') !== 'repo:push') {
+            return response()->json(['success'=>false,'message'=>'I only want push events :P.'], 200);
+        }
+
         $payload = json_decode( stripslashes( $request->get('payload') ), true );
 
-        $repository = $this->provider->repositories()->where('reference',$payload['repository']['slug'])->first();
+        if(!isset($payload['push']['changes'][0]['new'])) {
+            return response()->json(['success'=>false,'message'=>'No changes found.'], 200);
+        }
 
-        print '<pre>'; var_dump($payload); die();
+        $pushType = $payload['push']['changes'][0]['new']['type'];
+        // We only interested in tag push not branch.
+        // TODO: Option for branch or tag?
+        if($pushType !== 'tag' && $pushType !== ' annotated_tag') {
+            return response()->json(['success'=>false,'message'=>'No tag push found.'], 200);
+        }
+
+        $repository =   $this->provider
+                        ->repositories()
+                        ->where('reference',$payload['repository']['uuid'])
+                        ->first();
+
     }
 }
